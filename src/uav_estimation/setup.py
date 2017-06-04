f6a100dca3319cfc645ba86aef11f901fb953be7
kuya <kuya@gwu.edu>
2017-06-04 19:10:13 -0400

    clean up

--- src/uav_estimation/setup.py
+++ src/uav_estimation/setup.py
@@ -1,10 +1,2 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-setup_args = generate_distutils_setup(
-        name='estimation',
-        packages=['estimation'],
-        version='0.0.1',
-        package_dir = {'': 'ukf_uav'},
-    )
-setup(**setup_args)
+from setuptools import setup
+setup(name='ukf', packages=['ukf'])
--- src/uav_estimation/ukf_uav/ukf_uav.py
+++ src/uav_estimation/ukf_uav/ukf_uav.py
@@ -1,5 +1,4 @@
-#!/bin/bash/python
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import (absolute_import, division, print_function, unicode_literals)
 import numpy as np
 # from numba import jit, int16
 from numpy.linalg import cholesky, inv, det
